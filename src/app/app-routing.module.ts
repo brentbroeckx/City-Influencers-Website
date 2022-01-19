bdf0b8c874982a8c016c11dfe87a95f4c4491e79
Brent Broeckx <r0738889@student.thomasmore.be>
2022-01-19 14:43:25 +0100

    homepage layout & start dashboard layout + nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent}
+  { path: '', component: HomeComponent},
+  { path: 'dashboard', component: DashboardComponent}
 ];
 
 @NgModule({
