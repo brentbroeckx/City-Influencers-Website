dd96f0c0316a8ca4e5f678d7c355ce8beb01ca7f
Brent Broeckx <r0738889@student.thomasmore.be>
2022-01-22 02:43:06 +0100

    added basic auth and authGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { DashboardComponent } from './admin/dashboard/dashboard.component';
 import { MyInfluencersComponent } from './admin/dashboard/my-influencers/my-influencers.component';
 import { OverviewComponent } from './admin/dashboard/overview/overview.component';
 import { AppDownloadPageComponent } from './app-download-page/app-download-page.component';
+import { AuthGuard } from './auth/authGuard';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
@@ -15,8 +16,8 @@ const routes: Routes = [
   { path: 'download-page', component: AppDownloadPageComponent},
   { path: 'login', component: LoginComponent},
   { path: 'sign-up', component: SignUpComponent},
-  { path: 'dashboard/overview', component: OverviewComponent},
-  { path: 'dashboard/my-influencers', component: MyInfluencersComponent},
+  { path: 'dashboard/overview', component: OverviewComponent, canActivate: [AuthGuard]},
+  { path: 'dashboard/my-influencers', component: MyInfluencersComponent, canActivate: [AuthGuard]},
 
 ];
 
