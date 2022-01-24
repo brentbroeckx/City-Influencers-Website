db9412eb70e065e3562abfb797814138c4aa92cf
Ben <r0785673@student.thomasmore.be>
2022-01-24 15:04:03 +0100

    tab met tasks toegevoed + layout

--- src/app/admin/dashboard/my-tasks/my-tasks.module.ts
+++ src/app/admin/dashboard/my-tasks/my-tasks.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from "@angular/core";
 import { SharedModule } from "src/app/shared/shared.module";
 import { DashboardHeaderComponent } from "../dashboard-header/dashboard-header.component";
+import { DashboardModule } from "../dashboard-header/dashboard.module";
 import { SideNavigationComponent } from "../navigation/side-navigation.component";
+import { SidenavigationModule } from "../navigation/side-navigation.module";
 import { MyTasksRoutingModule } from "./my-tasks-routing.module";
 import { MyTasksComponent } from "./my-tasks-tab/my-tasks.component";
 
@@ -11,7 +13,9 @@ import { MyTasksComponent } from "./my-tasks-tab/my-tasks.component";
     ],
     imports: [
         SharedModule,
-        MyTasksRoutingModule
+        MyTasksRoutingModule,
+        DashboardModule,
+        SidenavigationModule
     ],
     exports: [
         MyTasksComponent,
