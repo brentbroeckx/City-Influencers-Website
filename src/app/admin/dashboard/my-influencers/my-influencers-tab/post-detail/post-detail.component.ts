12a4f18e268385b4af555ec7e64cf806041267d9
britt-theunis <r0785522@student.thomasmore.be>
2022-02-01 09:23:32 +0100

    privacy policy page

--- src/app/admin/dashboard/my-influencers/my-influencers-tab/post-detail/post-detail.component.ts
+++ src/app/admin/dashboard/my-influencers/my-influencers-tab/post-detail/post-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Post } from 'src/app/models/post';
 import { PostsService } from 'src/app/services/posts.service';
@@ -12,6 +13,10 @@ export class PostDetailComponent implements OnInit {
 
   post: Post = {id: "", influencerid: "", stadid: "", foto: "", beschrijving: "", isgoedgekeurd: "", commentaarstad: "", aantallikes: "", aantalcomments: "", bereik: "" }
 
+  postForm = new FormGroup({
+    approved: new FormControl('', [Validators.required]),
+    comment: new FormControl('', Validators.required)
+  })
 
   constructor(private postsService: PostsService, private route: ActivatedRoute) { }
 
@@ -23,6 +28,32 @@ export class PostDetailComponent implements OnInit {
           this.post = res.data[0];
       })
     }
+  }
+
+  onSubmit() {
+    console.log("registering button clicked")
+
+    var approved = this.postForm.controls.approved.value;
+    var comment = this.postForm.controls.comment.value;
+    console.log(approved, comment)
+
+
+    /* var cityRegister: CityRegister = {
+      username: this.registerForm.controls.username.value,
+      password: encryptedPass,
+      name: this.registerForm.controls.city.value,
+      postcode: this.registerForm.controls.postcode.value,
+      email: this.registerForm.controls.email.value,
+      type: "stad"
+    }
+
+    console.log("Registering...")
+    console.log(cityRegister)
+
+    this.signUpSerivce.processLogin(cityRegister).subscribe(res => {
+      console.log(res)
+    }); */
+
   }
 
 
