75dbb3046378a59c30b8ea30808c606b6c468fcd
britt-theunis <r0785522@student.thomasmore.be>
2022-02-01 13:05:07 +0100

    city account aanpassen

--- src/app/services/city.service.ts
+++ src/app/services/city.service.ts
@@ -5,6 +5,8 @@ import { City } from '../models/city';
 import { CityApiResponse } from '../models/cityApiResponse';
 
 import { environment } from 'src/environments/environment';
+import { CityRegister } from '../models/cityRegister';
+import { CityChange } from '../models/cityChange';
 
 @Injectable({
   providedIn: 'root'
@@ -45,6 +47,28 @@ export class CityService {
     })
 
     return this.httpClient.get<CityApiResponse>(environment.API_URL +"cities?where=isnew&like=t", {headers: headers})
+  }
+
+  changeCity(city: CityChange){
+    const changeParam = {
+      type: "stad",
+      id: city.id,
+      username: city.username,
+      name: city.name,
+      password: city.password,
+      postcode: city.postcode,
+      email: city.emailadres,
+      isactief: city.isactief
+    }
+    console.log(city)
+
+    const bearer = localStorage.getItem('token');
+
+    let headers = new HttpHeaders({
+      'Authorization': `Bearer ${bearer}`
+    })
+
+    return this.httpClient.put<any>( environment.API_URL + "accounts", changeParam, {headers: headers});
   }
 
   changeCityStatus(id: string, value: boolean) {
