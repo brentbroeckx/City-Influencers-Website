2902bdeffddb62ea150e8d4d13f99bd498420315
britt-theunis <r0785522@student.thomasmore.be>
2022-02-08 16:26:47 +0100

    admin toevoegen

---
+++ src/app/models/AdminCreate.ts
@@ -0,0 +1,7 @@
+export interface AdminCreate {
+    username: string;
+    firstname: string;
+    lastname: string;
+    email: number;
+    password: string;
+}
--- src/app/services/admin.service.ts
+++ src/app/services/admin.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { AdminCreate } from '../models/AdminCreate';
 
 @Injectable({
   providedIn: 'root'
@@ -17,5 +18,23 @@ export class AdminService {
     })
 
     //return this.httpClient.get<CityApiResponse>(environment.API_URL +"cities/", {headers: headers})
+  }
+
+  createAdmin(admin: AdminCreate){
+    const createParam = {
+      username: admin.username,
+      password: admin.password,
+      firstname: admin.firstname,
+      lastname: admin.lastname,
+      email: admin.email
+    }
+
+    const bearer = localStorage.getItem('token');
+
+    let headers = new HttpHeaders({
+      'Authorization': `Bearer ${bearer}`
+    })
+
+    return this.httpClient.post<any>( environment.API_URL + "accounts", createParam, {headers: headers});
   }
 }
